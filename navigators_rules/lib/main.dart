6e6dbf356203f3231c5a2741e153507bc549240e
oca <mukhammadanshori11@gmail.com>
2026-04-11 18:17:32 +0000

    multiilayout

--- navigators_rules/lib/main.dart
+++ navigators_rules/lib/main.dart
@@ -94,3 +94,55 @@ class SingleChildExample extends StatelessWidget {
     );
   }
 }
+
+//colum
+class ColumnExample extends StatelessWidget {
+  const ColumnExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Contoh Column')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Ini baris pertama'),
+            Text('Ini baris kedua'),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Tombol'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//stack
+class StackExample extends StatelessWidget {
+  const StackExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Contoh Stack')),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(width: 200, height: 200, color: Colors.blue[100]),
+              Container(width: 150, height: 150, color: Colors.blue[300]),
+              Text(
+                'Tumpuk!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
